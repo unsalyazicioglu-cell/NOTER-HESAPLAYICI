b97672c0b4a02b2e7eb2f2a40114928f81a66e00
unsalyazicioglu-cell <unsal.yazicioglu@gmail.com>
2026-06-16 22:53:26 +0300

    Update degerle.js

--- api/degerle.js
+++ api/degerle.js
@@ -29,7 +29,7 @@ SADECE JSON DÖNDÜR.
 console.log("API KEY VAR MI:", !!process.env.GEMINI_API_KEY);
 console.log("VERI:", veri);
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
